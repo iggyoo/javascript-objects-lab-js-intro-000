53eb3f85ab51f63a4b74a6a3c7df6df759d1330a
Scarlett Baez <s.baez1010@gmail.com>
2020-06-08 22:14:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function  updateObjectWithKeyAndValue(object, key, value) {
-var update = {prop: '1', prop2: '2'};
-return Object.assign({}, oject, { [key]: value })
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
 }
