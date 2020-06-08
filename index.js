f3999e4434b8d6e7d1f1ddc2910b243531ded339
Scarlett Baez <s.baez1010@gmail.com>
2020-06-08 22:51:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function deleteFromObjectByKey(object, key, value) {
   return Object.assign({}, object, { [key]: value })
   }
 
-function destructivelyDeleteFromObjectByKey(object, key, value,) {
+function destructivelyDeleteFromObjectByKey(object, key, value) {
 return Object.assign({}, object, { [key]: value })
 }
