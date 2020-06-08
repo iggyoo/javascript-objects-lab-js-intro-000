0a82ccaba5d8850866ef3c257da0ce1f4ad5e886
Scarlett Baez <s.baez1010@gmail.com>
2020-06-08 22:13:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function  updateObjectWithKeyAndValue(object, key, value) {
 var update = {prop: '1', prop2: '2'};
-return update
-update.prop = "new value"
+return Object.assign({}, oject, { [key]: value })
 }
