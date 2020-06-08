701484906607d1fc22d217b61e3a260c108fdf3f
Scarlett Baez <s.baez1010@gmail.com>
2020-06-08 22:26:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-return Object.assign({}, object, { [key]: value})
+obj[key] = value
+
+return obj
 }
